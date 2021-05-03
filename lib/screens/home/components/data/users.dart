01a0be139a8bb4db616ca7360e79f3ec802c990c
ADmin <ngle2211>
2021-05-04 01:04:16 +0700

    commit

---
+++ lib/screens/home/components/data/users.dart
@@ -0,0 +1,63 @@
+import '../model/user.dart';
+
+final dummyUsers = [
+  User(
+      name: 'Lauren Turner',
+      designation: 'Content Writer',
+      mutualFriends: 4,
+      bio:
+      'Psychology, science, and art are what helps me to learn the outside world and myself.',
+      age: 24,
+      imgUrl: 'assets/images/chat_image1.jpg',
+      location: 'North London'),
+  User(
+    name: 'Lori Perez',
+    designation: 'UI Designer ',
+    mutualFriends: 8,
+    bio:
+    'Travelling, adventures, extreme sports are also an integral part of me, but I like watching and admiring extreme sports rather than doing it ?',
+    location: 'Leeds',
+    age: 26,
+    imgUrl: 'assets/images/chat_image2.jpg',
+  ),
+  User(
+    name: 'Christine Wallace',
+    designation: 'News Reporter',
+    mutualFriends: 2,
+    bio:
+    'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Liverpool',
+    age: 23,
+    imgUrl: 'assets/images/chat_image3.jpg',
+  ),
+  User(
+    name: 'Rachel Green',
+    designation: 'Architect',
+    mutualFriends: 8,
+    bio:
+    'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Nottingham',
+    age: 22,
+    imgUrl: 'assets/images/chat_image4.jpg',
+  ),
+  User(
+    name: 'Emma',
+    designation: 'Software Developer',
+    mutualFriends: 3,
+    bio:
+    'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Manchester',
+    age: 25,
+    imgUrl: 'assets/images/chat_image4.jpg',
+  ),
+  User(
+    name: 'Kim Wexler',
+    designation: 'Accountant',
+    mutualFriends: 5,
+    bio:
+    'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Birmingham',
+    age: 30,
+    imgUrl: 'assets/images/chat_image5.png',
+  ),
+];
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:cupet/components/custom_bottom_nav_bar.dart';
 import 'package:cupet/enums.dart';
+import 'components/HomePage.dart';
 
-import 'components/body.dart';
+import 'package:provider/provider.dart';
+import 'package:cupet/screens/home/components/HomePage.dart';
+import 'package:cupet/screens/home/components/provider/feedback_position_provider.dart';
+
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
+
+  // @override
+  // Widget build(BuildContext context) => ChangeNotifierProvider(
+  //     create: (context) => FeedbackPositionProvider() {
+  //   return Scaffold(
+  //     body: HomePage(),
+  //     bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+  //   );
+  // };
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Body(),
-      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => FeedbackPositionProvider(),
+      child: Scaffold(
+        body: HomePage(),
+        bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+      )
+  );
 }
