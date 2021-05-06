35ded61eca190cc071504f6d2db0e0f60a817c3e
ngle2211 <47818047+ngle2211@users.noreply.github.com>
2021-05-06 17:14:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cupet/routes.dart';
-import 'package:cupet/screens/profile/profile_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:cupet/screens/splash/splash_screen.dart';
 import 'package:cupet/theme.dart';
+import 'package:cupet/screens/home/components/provider/feedback_position_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +12,16 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: theme(),
-      // home: SplashScreen(),
-      // We use routeName so that we dont need to remember the name
-      initialRoute: SplashScreen.routeName,
-      routes: routes,
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => FeedbackPositionProvider(),
+      child:  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Cupet',
+        theme: theme(),
+        // home: SplashScreen(),
+        // We use routeName so that we dont need to remember the name
+        initialRoute: SplashScreen.routeName,
+        routes: routes,
+      )
+  );
 }
